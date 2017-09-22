b6721e20d8f5d020ff275527d529c500267967d5
chenlong <chenlongwill@qq.com>
2017-09-22 13:40:26 +0800

    上传图片成功

--- src/pages/activities.vue
+++ src/pages/activities.vue
@@ -3,6 +3,7 @@
         <div id="page-inner">
           <ul class="list-inline">
               <li><h3>活动专区管理</h3></li>
+              <li class="pull-right"><button class="btn btn-danger" @click="delImgs()">删除全部图片</button></li>
           </ul>
           <hr/>
           <div style="margin:0 auto; padding:20px;">
@@ -11,15 +12,34 @@
                        <span class="required">*</span>图片:
                     </div>
                     <div class="col-sm-9 col-md-9 col-xs-6">
-                         <ul class="list-inline">
-                          <li><img v-bind:src="activities.imgurl" id="file" class="profile"/></li>
-                          <li style="position:relative;">
-                            <input type="file" @change="onFileChange" ref="upload" value="上传图片" style="position:absolute; opacity:0;"/>
-                            <button style="background-color:#84B4DC; color:#fff; border:1px solid transparent; padding:5px 10px;" >
+                          <ul class="list-inline">
+                            <li v-for="item in activities"><img v-bind:src="item"  /></li>
+                          </ul>
+                          <div class="img-inner">
+                              <div class="img_div" id="img_inner" style="display: inline-block; vertical-align: top;">
+
+                               </div>
+                              <a class="a-upload">
+                                  <input type="file" name="myFile" id="myFile" @change="profileReview" multiple="multiple" />
+                                  <img src="../../static/images/authen.png" alt="">
+                              </a>
+                              <div class="shade" @click="closeShade()">
+                                  <div class="">
+                                      <span class="text_span">
+
+                                      </span>
+                                  </div>
+                              </div>
+
+                              <div class="shadeImg" @click="closeShadeImg()">
+                                  <div class="">
+                                      <img class="showImg" src="">
+                                  </div>
+                              </div>
+                          </div>
+                         <button style="background-color:#84B4DC; color:#fff; border:1px solid transparent; padding:5px 10px;" @click="uploadImgs" >
                                 上传图片
-                            </button>
-                          </li>
-                      </ul>
+                         </button>
                     </div>
                 </div>
             </div>
@@ -41,7 +61,8 @@ export default {
   data (){
     return {
         Sid:'',
-        activities:{},
+        activities:[],
+        ImgArr:[],  //上传图片的数组
     }
   },
   mounted (){
@@ -66,12 +87,21 @@ export default {
         console.log(err);
       });
     },
+
+
     initData(){
       let that = this;
       api.activityQuery().then(function(res){
         if(res.data.Code ==3){
              let templateObj= res.data.Data;
-             that.activities= templateObj[0];
+
+             let Imgs = templateObj[0].imgurl.split(";");
+
+             console.log(Imgs);
+
+             for(let i = 0; i<Imgs.length-1;i++){
+                that.activities.push(Imgs[i]);
+             }
         }
       }).catch(function(err){
           console.log(err);
@@ -85,7 +115,7 @@ export default {
            this.createImage(files[0]);
      },
 
-     createImage(file) {
+    createImage(file) {
         var image = new Image();
         var reader = new FileReader();
         var that = this;
@@ -107,7 +137,7 @@ export default {
         data.append('img', input.files[0]);
         data.append('sid',this.Sid);
 
-        axios.post(env.baseUrl+'/cycj/activity/update', data, {
+        axios.post(env.baseUrl+'/yyt/activity/update', data, {
             headers: {
                   'Content-Type': 'application/x-www-form-urlencoded'
             }
@@ -118,6 +148,116 @@ export default {
         .catch(function (error) {
           console.log(error);
         });
+    },
+
+
+    profileReview(e) {
+        var objUrl;
+        var img_html;
+
+        var img_div = $("#img_inner");
+
+        var filepath = $("input[name='myFile']").val();
+
+        var files = e.target.files || e.dataTransfer.files;
+
+        for(var i = 0; i < files.length; i++) {
+            objUrl = this.getObjectURL(files[i]);
+            var extStart = filepath.lastIndexOf(".");
+            var ext = filepath.substring(extStart, filepath.length).toUpperCase();
+            /*
+             描述：鉴定每个图片上传尾椎限制
+             * */
+            if(ext != ".BMP" && ext != ".PNG" && ext != ".GIF" && ext != ".JPG" && ext != ".JPEG") {
+                $(".shade").fadeIn(500);
+                $(".text_span").text("图片限于bmp,png,gif,jpeg,jpg格式");
+                this.ImgArr=[];
+                return false;
+            } else {
+                /*
+                 若规则全部通过则在此提交url到后台数据库
+                 * */
+                 img_html = "<div class='isImg' style='display:inline-block;'>" +
+                        "<img src='" + objUrl + "' style='height:300px; width: auto;' />" +
+                        "</div>";
+                    img_div.append(img_html);
+                this.ImgArr.push(files[i]);
+            }
+        }
+        return true;
+    },
+
+
+    uploadImgs(){
+      if(this.ImgArr.length>1){
+         var data = new FormData();
+
+        data.append('imgs', this.ImgArr.length);
+
+        data.append('sid',this.Sid);
+
+        for(let i=0; i<this.ImgArr.length;i++){
+            let idx = i+1;
+            let Img = 'img'+idx;
+
+            data.append(Img,this.ImgArr[i]);
+        }
+
+        axios.post(env.baseUrl+'/yyt/activity/update', data, {
+              headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+              }
+          })
+          .then(function (res) {
+            if(res.data.Code ==3){
+              window.location.reload();
+            }
+            alert(res.data.Msg);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+        }else{
+          alert("尚未上传图片!");
+        }
+    },
+
+    delImgs(){
+        var data = new FormData();
+
+        data.append('imgs', 0);
+
+        data.append('sid',this.Sid);
+
+        axios.post(env.baseUrl+'/yyt/activity/update', data, {
+              headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+              }
+        })
+        .then(function (res) {
+          if(res.data.Code ==3){
+              window.location.reload();
+            }
+          alert(res.data.Msg);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+
+      /*
+     描述：鉴定每个浏览器上传图片url 目前没有合并到Ie
+     * */
+    getObjectURL(file) {
+      var url = null;
+      if(window.createObjectURL != undefined) { // basic
+          url = window.createObjectURL(file);
+      } else if(window.URL != undefined) { // mozilla(firefox)
+          url = window.URL.createObjectURL(file);
+      } else if(window.webkitURL != undefined) { // webkit or chrome
+          url = window.webkitURL.createObjectURL(file);
+      }
+      return url;
     },
   },
 }
@@ -135,4 +275,34 @@ export default {
         margin-right:5px;
     }
 
+    .img-inner{
+      width:100%;
+      height:auto;
+    }
+
+    .img-inner .isImg img{
+        height:300px;
+
+    }
+
+    .isImg{
+      display:inline-block;
+    }
+
+    .a-upload{
+      width:165px;
+      height:147px;
+      overflow:hidden;
+      position:relative;
+      display:block;
+    }
+
+    .a-upload input{
+      position:absolute;
+      left:0;
+      width:100%;
+      height:100%;
+      top:50%;
+      opacity:0;
+    }
 </style>
